ac6c133a9bdec967a81a4b0628b9cc0308b0cc85
glebushnik <glebushnik@MacBook-Pro-glebushnik.local>
2024-06-29 20:43:06 +0300

    fix

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ async def button(update: Update, context: CallbackContext) -> int:
     elif current_field == "location":
         context.user_data["location"] = selected_value
     elif current_field == "subject_area":
-        if selected_value == "next":
+        if selected_value == "Следующий пункт анкеты":
             if len(context.user_data["selected_subject_areas"]) == 0:
                 await update.callback_query.message.reply_text(
                     "Пожалуйста, выберите хотя бы одну предметную область."
@@ -100,15 +100,17 @@ async def button(update: Update, context: CallbackContext) -> int:
             return await request_next_field(update, context)
         else:
             if selected_value not in context.user_data["selected_subject_areas"]:
-                context.user_data["selected_subject_areas"].append(selected_value)
-                selected_subject_areas = ", ".join(context.user_data["selected_subject_areas"])
-                await update.callback_query.message.reply_text(
-                    f"Выбранные предметные области: {selected_subject_areas}"
-                )
-            if len(context.user_data["selected_subject_areas"]) == 3:
-                context.user_data["subject_area"] = ", ".join(context.user_data["selected_subject_areas"])
-                context.user_data["current_field_index"] += 1
-                return await request_next_field(update, context)
+                if len(context.user_data["selected_subject_areas"]) < 3:
+                    context.user_data["selected_subject_areas"].append(selected_value)
+                    selected_subject_areas = ", ".join(context.user_data["selected_subject_areas"])
+                    await update.callback_query.message.reply_text(
+                        f"Выбранные предметные области: {selected_subject_areas}"
+                    )
+                else:
+                    await update.callback_query.message.reply_text(
+                        "Вы не можете выбрать более 3 предметных областей."
+                    )
+                    return AWAITING_FIELD
 
     # Переход к следующему полю только если не выбран "subject_area"
     if current_field != "subject_area":
@@ -116,7 +118,6 @@ async def button(update: Update, context: CallbackContext) -> int:
         return await request_next_field(update, context)
 
     return AWAITING_FIELD
-
 
 
 async def request_next_field(update: Update, context: CallbackContext) -> int:
